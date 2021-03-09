1ca9bb073b269abbb372ad65a38da4f1f1bb0ed9
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-03-09 09:13:11 -0600

    navbar for videos - view for videos - more

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+import cards.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cards.urls')),
     path("admin/", admin.site.urls),
+    path("", include("cards.urls")),
+    path("videos/", cards.views.VideoView.as_view(), name="videos"),
 ]
