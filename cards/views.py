169de7cf707a1e00be54dffd55155cd438951dc5
Gregory Bowles <sbowles21@basecampcodingacademy.org>
2021-03-09 09:37:33 -0600

    fixing login and signup

--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -8,3 +11,8 @@ class HomeView(TemplateView):
 
 class VideoView(TemplateView):
     template_name = "videos.html"
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
