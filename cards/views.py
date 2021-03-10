44fa561e8df1492176cc07e6a152d27f23e7e2fc
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-03-10 09:18:32 -0600

    model changes and signup fix

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -8,13 +8,16 @@ from django.views import generic
 class HomepageView(TemplateView):
     template_name = "homepage.html"
 
+
 class HomeView(TemplateView):
     template_name = "home.html"
+
 
 class VideoView(TemplateView):
     template_name = "videos.html"
 
+
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
