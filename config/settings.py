dfaf4ed350220dea92474e2cb499b228cb64e2c5
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-03-18 10:03:21 -0500

    heroku dumb

--- config/settings.py
+++ config/settings.py
@@ -22,14 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
+DEBUG = env.bool("DEBUG", default=False)
 
+
+SECRET_KEY = env.str("SECRET_KEY")
 # Application definition
 
 INSTALLED_APPS = [
